53e2563c1fdfa3233b3f66d5f48149e71f200b79
Anderson Vieira <andersonvnascimento529@gmail.com>
2024-07-27 09:51:36 -0300

    Add: The second exercise

---
+++ class01_bootcamp/example_02.py
@@ -0,0 +1,7 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+first_value = int(input('Write the first value: '))
+second_value = int(input('Write the second value: '))
+sum = first_value + second_value
+
+print(f"The sum of {first_value} with {second_value} is equal to: {sum}")
