8cdeeb7fef0ee45284fac81ad95b3324826e0af9
Anderson Vieira <andersonvnascimento529@gmail.com>
2024-07-27 09:51:59 -0300

    Add: The third exercise

---
+++ class01_bootcamp/example_03.py
@@ -0,0 +1,7 @@
+# Exercício 03: Refatore o exercício 02 atribuindo variáveis
+
+first_value = int(input('Write the first value: '))
+second_value = int(input('Write the second value: '))
+sum = first_value + second_value
+
+print(f"The sum of {first_value} with {second_value} is equal to: {sum}")
